77cc0a8da6615c4f477f21031567987cdd91c18a
irsan mansyur <irsan00mansyur@gmail.com>
2023-02-07 04:28:31 +0700

    Initial commit

---
+++ tests/common.test.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import { render } from '@testing-library/react'
+import 'jest-canvas-mock'
+import { Tooltip } from '../src'
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<Tooltip text='Hello kamu' />)
+  })
+})
